c80e7f7aaa32cd21085928cafb3ac20194966bf3
d.vishnivetskiy <danilvisn@list.ru>
2020-12-28 22:51:39 +0300

    test graphics

--- src/ru/Daniilscream/tanks/display/Display.java
+++ src/ru/Daniilscream/tanks/display/Display.java
@@ -4,6 +4,14 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import java.util.Arrays;
+
+import javax.swing.Box.Filler;
 
 import javax.swing.JFrame;
 
@@ -13,25 +21,26 @@ public abstract class Display {
 	private static JFrame window;
 	private static Canvas content;
 	
-	public static void create(int width, int height, String title) {
+	private static BufferedImage buffered;
+	private static int[] bufferData;
+	private static Graphics bufferGraphics;
+	private static int clearColor;
+	
+	private static BufferStrategy bufferStrategy;
+	
+	private static float delta = 0;
+	
+	public static void create(int width, int height, String title, int _clearColor, int numBuffers) {
 		
 		if(created)
 			return;
 		
 		window = new JFrame(title);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		content = new Canvas() {
-			
-			@Override
-			public void paint(Graphics g) {
-				super.paint(g);
-				render(g);
-			}
-		};
+		content = new Canvas();
 		
 		Dimension size = new Dimension(width, height);
 		content.setPreferredSize(size);
-		content.setBackground(Color.black);
 		
 		window.setResizable(false);
 		window.getContentPane().add(content);
@@ -39,15 +48,34 @@ public abstract class Display {
 		window.setLocationRelativeTo(null);
 		window.setVisible(true);
 		
+		buffered = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		bufferData = ((DataBufferInt)buffered.getRaster().getDataBuffer()).getData();
+		bufferGraphics = buffered.getGraphics();
+		clearColor = _clearColor;
+		
+		content.createBufferStrategy(numBuffers);
+		bufferStrategy = content.getBufferStrategy();
+		
+		created = true;
+	}
+	
+	public static void clear() {
+		Arrays.fill(bufferData, clearColor);
 	}
 	
 	public static void render() {
-		content.repaint();
+		bufferGraphics.setColor(new Color(0xff501270));
+		bufferGraphics.fillOval((int)(350+(Math.sin(delta)*200)), 250, 100, 100);
+		
+		((Graphics2D)bufferGraphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		bufferGraphics.fillOval((int)(500+(Math.sin(delta)*200)), 250, 100, 100);
+		((Graphics2D)bufferGraphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
+//		delta+=0.02f;
 	}
 	
-	private static void render(Graphics g) {
-		g.setColor(Color.white);
-		g.fillOval(400-50, 300-50, 100, 100);
+	public static void swapBuffers() {
+		Graphics g = bufferStrategy.getDrawGraphics();
+		g.drawImage(buffered, 0,0,null);
+		bufferStrategy.show();
 	}
-
 }
--- src/ru/Daniilscream/tanks/main/Main.java
+++ src/ru/Daniilscream/tanks/main/Main.java
@@ -11,13 +11,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Display.create(800, 600, "Tanks");
+		Display.create(800, 600, "Tanks", 0xff00ff00, 3);
 		
 		Timer t = new Timer(1000/60, new AbstractAction() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				Display.clear();
 				Display.render();
+				Display.swapBuffers();
 			}
 		});
 			t.setRepeats(true);
